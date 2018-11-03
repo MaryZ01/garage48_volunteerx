b0aeea44afa1b698af5a7c26785be051cd924c38
Anastasiia K <anonymusua99@gmail.com>
2018-11-03 20:35:32 +0200

    commands upd

---
+++ VolunteerX/Models/Commands/CustomizeInfoCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace VolunteerX.Models.Commands
+{
+    public class CustomizeInfoCommand : Command
+    {
+        public override string Name => "Customise information";
+
+        public override bool Contains(Telegram.Bot.Types.Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Telegram.Bot.Types.Message message, TelegramBotClient client)
+        {
+            throw new NotImplementedException();
+            //TODO: USER MUST BE ABLE TO CASTOMISE INFO ABOUT HIS PROFILE
+        }
+    }
+}
---
+++ VolunteerX/Models/Commands/MyEventsHistoryCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace VolunteerX.Models.Commands
+{
+    public class MyEventsHistoryCommand : Command
+    {
+        public override string Name => "My events history";
+
+        public override bool Contains(Telegram.Bot.Types.Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Telegram.Bot.Types.Message message, TelegramBotClient client)
+        {
+            //TODO: REALISE EVENTS HISTORY
+            throw new NotImplementedException();
+        }
+    }
+}
